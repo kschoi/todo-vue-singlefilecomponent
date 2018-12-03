351c41e306861763a571d3319b4a3d91bf52c74e
kyungsun choi <kschoi0308@gmail.com>
2018-12-03 15:22:26 +0900

    리팩토링

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,35 @@
 <template>
   <div id="app" class="todoapp">
     <!-- todo header -->
-		<header class="header">
-			<h1>todos</h1>
-			<input class="new-todo"
-				autofocus autocomplete="off"
-				placeholder="할 일을 적어주세요!"
-				v-model="newTodo"
-				@keyup.enter="addTodo">
-		</header>
+    <todo-header @add:todo="addTodo"></todo-header>
     <!-- //todo header -->
-    <!-- todo body -->
-		<section class="main" v-show="todos.length" v-cloak>
-			<input class="toggle-all" type="checkbox" v-model="allDone">
-			<ul class="todo-list">
-				<li v-for="todo in filteredTodos"
-					class="todo"
-					:key="todo.id"
-					:class="{ completed: todo.completed, editing: todo == editedTodo }">
-					<div class="view">
-						<input class="toggle" type="checkbox" v-model="todo.completed">
-						<label @dblclick="editTodo(todo)">{{ todo.title }}</label>
-						<button class="destroy" @click="removeTodo(todo)"></button>
-					</div>
-					<input class="edit" type="text"
-						v-model="todo.title"
-						v-todo-focus="todo == editedTodo"
-						@blur="doneEdit(todo)"
-						@keyup.enter="doneEdit(todo)"
-						@keyup.esc="cancelEdit(todo)">
-				</li>
-			</ul>
-		</section>
-    <!-- todo body -->
+    <!-- todo list -->
+		<todo-list
+			:todos="todos"
+			:filteredTodos="filteredTodos"
+			:remaining="remaining"
+			@edit:todo="editTodo"
+			@remove:todo="removeTodo"
+			@done:edit="doneEdit"
+			@cancel:edit="cancelEdit">
+		</todo-list>
+    <!-- todo list -->
     <!-- todo footer -->
-		<footer class="footer" v-show="todos.length" v-cloak>
-			<span class="todo-count">
-				<strong>{{ remaining }}</strong>개의 할일이 있습니다.
-			</span>
-			<ul class="filters">
-				<li><a href="#/all" @click="onHashChange('all')" :class="{ selected: visibility == 'all' }">All</a></li>
-				<li><a href="#/active" @click="onHashChange('active')" :class="{ selected: visibility == 'active' }">Active</a></li>
-				<li><a href="#/completed" @click="onHashChange('completed')" :class="{ selected: visibility == 'completed' }">Completed</a></li>
-			</ul>
-			<button class="clear-completed" @click="removeCompleted" v-show="todos.length > remaining">
-				완료된 일 삭제
-			</button>
-		</footer>
+		<todo-footer
+			:todos="todos"
+			:visibility="visibility"
+			:remaining="remaining"
+			@remove:completed="removeCompleted">
+		</todo-footer>
     <!-- //todo footer -->
   </div>
 </template>
 
 <script>
+import TodoHeader from './components/TodoHeader.vue';
+import TodoList from './components/TodoList.vue';
+import TodoFooter from './components/TodoFooter.vue';
+
 // localStorage persistence
 const STORAGE_KEY = 'todos-vuejs-1.0'
 const todoStorage = {
@@ -85,23 +63,21 @@ const filters = {
 }
 
 export default {
-  name: 'app',
+	name: 'app',
+	
   data () {
     return {
       todos: todoStorage.fetch(),
-      newTodo: '',
-      editedTodo: null,
       visibility: 'all'
     }
-  },
-  watch: {
-    todos: {
-      handler (todos) {
-        todoStorage.save(todos)
-      },
-      deep: true
-    }
-  },
+	},
+
+	components: {
+		'todo-header': TodoHeader,
+    'todo-list': TodoList,
+    'todo-footer': TodoFooter,
+	},
+
   computed: {
     filteredTodos () {
       return filters[this.visibility](this.todos)
@@ -109,29 +85,15 @@ export default {
     remaining () {
       return filters.active(this.todos).length
     },
-    allDone: {
-      get () {
-        return this.remaining === 0
-      },
-      set (value) {
-        this.todos.forEach(function (todo) {
-          todo.completed = value
-        })
-      }
-    }
-  },
+	},
+	
   methods: {
-    addTodo () {
-      var value = this.newTodo && this.newTodo.trim()
-      if (!value) {
-        return
-      }
+    addTodo (value) {
       this.todos.push({
         id: todoStorage.uid++,
         title: value,
         completed: false
       })
-      this.newTodo = ''
     },
 
     onHashChange (filter) {
@@ -146,14 +108,9 @@ export default {
 
     editTodo (todo) {
       this.beforeEditCache = todo.title
-      this.editedTodo = todo
     },
 
     doneEdit (todo) {
-      if (!this.editedTodo) {
-        return
-      }
-      this.editedTodo = null
       todo.title = todo.title.trim()
       if (!todo.title) {
         this.removeTodo(todo)
@@ -161,36 +118,42 @@ export default {
     },
 
     cancelEdit (todo) {
-      this.editedTodo = null
       todo.title = this.beforeEditCache
     },
 
     removeCompleted () {
       this.todos = filters.active(this.todos)
+		},
+		
+		changeFilter () {
+			let visibility = window.location.hash.replace(/#\/?/, '')
+			if (filters[visibility]) {
+				this.visibility = visibility
+			} else {
+				window.location.hash = ''
+				this.visibility = 'all'
+			}
+
+		}
+	},
+	
+  watch: {
+    todos: {
+      handler (todos) {
+        todoStorage.save(todos)
+      },
+      deep: true
     }
-  },
-  directives: {
-    'todo-focus': function (el, binding) {
-      if (binding.value) {
-        el.focus()
-      }
-    }
-  }
+	},
+
+	created () {
+		window.addEventListener('hashchange', this.changeFilter);
+	},
+
+	beforeDestroy () {
+		window.removeEventListener('hashchange', this.changeFilter);
+	},
 }
-
-// handle routing
-// function onHashChange () {
-//   var visibility = window.location.hash.replace(/#\/?/, '')
-//   if (filters[visibility]) {
-//     app.visibility = visibility
-//   } else {
-//     window.location.hash = ''
-//     app.visibility = 'all'
-//   }
-// }
-
-// window.addEventListener('hashchange', onHashChange);
-// onHashChange();
 
 </script>
 
@@ -262,308 +225,6 @@ body {
 	font-style: italic;
 	font-weight: 300;
 	color: #e6e6e6;
-}
-
-.todoapp h1 {
-	position: absolute;
-	top: -155px;
-	width: 100%;
-	font-size: 100px;
-	font-weight: 100;
-	text-align: center;
-	color: rgba(175, 47, 47, 0.15);
-	-webkit-text-rendering: optimizeLegibility;
-	-moz-text-rendering: optimizeLegibility;
-	text-rendering: optimizeLegibility;
-}
-
-.new-todo,
-.edit {
-	position: relative;
-	margin: 0;
-	width: 100%;
-	font-size: 24px;
-	font-family: inherit;
-	font-weight: inherit;
-	line-height: 1.4em;
-	border: 0;
-	color: inherit;
-	padding: 6px;
-	border: 1px solid #999;
-	box-shadow: inset 0 -1px 5px 0 rgba(0, 0, 0, 0.2);
-	box-sizing: border-box;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-}
-
-.new-todo {
-	padding: 16px 16px 16px 60px;
-	border: none;
-	background: rgba(0, 0, 0, 0.003);
-	box-shadow: inset 0 -2px 1px rgba(0,0,0,0.03);
-}
-
-.main {
-	position: relative;
-	z-index: 2;
-	border-top: 1px solid #e6e6e6;
-}
-
-label[for='toggle-all'] {
-	display: none;
-}
-
-.toggle-all {
-	position: absolute;
-	top: -55px;
-	left: -12px;
-	width: 60px;
-	height: 34px;
-	text-align: center;
-	border: none; /* Mobile Safari */
-}
-
-.toggle-all:before {
-	content: '❯';
-	font-size: 22px;
-	color: #e6e6e6;
-	padding: 10px 27px 10px 27px;
-}
-
-.toggle-all:checked:before {
-	color: #737373;
-}
-
-.todo-list {
-	margin: 0;
-	padding: 0;
-	list-style: none;
-}
-
-.todo-list li {
-	position: relative;
-	font-size: 24px;
-	border-bottom: 1px solid #ededed;
-}
-
-.todo-list li:last-child {
-	border-bottom: none;
-}
-
-.todo-list li.editing {
-	border-bottom: none;
-	padding: 0;
-}
-
-.todo-list li.editing .edit {
-	display: block;
-	width: 506px;
-	padding: 12px 16px;
-	margin: 0 0 0 43px;
-}
-
-.todo-list li.editing .view {
-	display: none;
-}
-
-.todo-list li .toggle {
-	text-align: center;
-	width: 40px;
-	/* auto, since non-WebKit browsers doesn't support input styling */
-	height: auto;
-	position: absolute;
-	top: 0;
-	bottom: 0;
-	margin: auto 0;
-	border: none; /* Mobile Safari */
-	-webkit-appearance: none;
-	appearance: none;
-}
-
-.todo-list li .toggle:after {
-	content: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="-10 -18 100 135"><circle cx="50" cy="50" r="50" fill="none" stroke="#ededed" stroke-width="3"/></svg>');
-}
-
-.todo-list li .toggle:checked:after {
-	content: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="-10 -18 100 135"><circle cx="50" cy="50" r="50" fill="none" stroke="#bddad5" stroke-width="3"/><path fill="#5dc2af" d="M72 25L42 71 27 56l-4 4 20 20 34-52z"/></svg>');
-}
-
-.todo-list li label {
-	word-break: break-all;
-	padding: 15px 60px 15px 15px;
-	margin-left: 45px;
-	display: block;
-	line-height: 1.2;
-	transition: color 0.4s;
-}
-
-.todo-list li.completed label {
-	color: #d9d9d9;
-	text-decoration: line-through;
-}
-
-.todo-list li .destroy {
-	display: none;
-	position: absolute;
-	top: 0;
-	right: 10px;
-	bottom: 0;
-	width: 40px;
-	height: 40px;
-	margin: auto 0;
-	font-size: 30px;
-	color: #cc9a9a;
-	margin-bottom: 11px;
-	transition: color 0.2s ease-out;
-}
-
-.todo-list li .destroy:hover {
-	color: #af5b5e;
-}
-
-.todo-list li .destroy:after {
-	content: '×';
-}
-
-.todo-list li:hover .destroy {
-	display: block;
-}
-
-.todo-list li .edit {
-	display: none;
-}
-
-.todo-list li.editing:last-child {
-	margin-bottom: -1px;
-}
-
-.footer {
-	color: #777;
-	padding: 10px 15px;
-	height: 20px;
-	text-align: center;
-	border-top: 1px solid #e6e6e6;
-}
-
-.footer:before {
-	content: '';
-	position: absolute;
-	right: 0;
-	bottom: 0;
-	left: 0;
-	height: 50px;
-	overflow: hidden;
-	box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2),
-	            0 8px 0 -3px #f6f6f6,
-	            0 9px 1px -3px rgba(0, 0, 0, 0.2),
-	            0 16px 0 -6px #f6f6f6,
-	            0 17px 2px -6px rgba(0, 0, 0, 0.2);
-}
-
-.todo-count {
-	float: left;
-	text-align: left;
-}
-
-.todo-count strong {
-	font-weight: 300;
-}
-
-.filters {
-	margin: 0;
-	padding: 0;
-	list-style: none;
-	position: absolute;
-	right: 0;
-	left: 0;
-}
-
-.filters li {
-	display: inline;
-}
-
-.filters li a {
-	color: inherit;
-	margin: 3px;
-	padding: 3px 7px;
-	text-decoration: none;
-	border: 1px solid transparent;
-	border-radius: 3px;
-}
-
-.filters li a:hover {
-	border-color: rgba(175, 47, 47, 0.1);
-}
-
-.filters li a.selected {
-	border-color: rgba(175, 47, 47, 0.2);
-}
-
-.clear-completed,
-html .clear-completed:active {
-	float: right;
-	position: relative;
-	line-height: 20px;
-	text-decoration: none;
-	cursor: pointer;
-}
-
-.clear-completed:hover {
-	text-decoration: underline;
-}
-
-.info {
-	margin: 65px auto 0;
-	color: #bfbfbf;
-	font-size: 10px;
-	text-shadow: 0 1px 0 rgba(255, 255, 255, 0.5);
-	text-align: center;
-}
-
-.info p {
-	line-height: 1;
-}
-
-.info a {
-	color: inherit;
-	text-decoration: none;
-	font-weight: 400;
-}
-
-.info a:hover {
-	text-decoration: underline;
-}
-
-/*
-	Hack to remove background from Mobile Safari.
-	Can't use it globally since it destroys checkboxes in Firefox
-*/
-@media screen and (-webkit-min-device-pixel-ratio:0) {
-	.toggle-all,
-	.todo-list li .toggle {
-		background: none;
-	}
-
-	.todo-list li .toggle {
-		height: 40px;
-	}
-
-	.toggle-all {
-		-webkit-transform: rotate(90deg);
-		transform: rotate(90deg);
-		-webkit-appearance: none;
-		appearance: none;
-	}
-}
-
-@media (max-width: 430px) {
-	.footer {
-		height: 50px;
-	}
-
-	.filters {
-		bottom: 10px;
-	}
 }
 
 [v-cloak] { display: none; }
---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,69 @@
+<template>
+    <header class="header">
+        <h1>todos</h1>
+        <input class="new-todo"
+            autofocus autocomplete="off"
+            placeholder="할 일을 적어주세요!"
+            v-model="newTodo"
+            @keyup.enter="addTodo">
+    </header>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            newTodo: '',
+        }
+    },
+
+    methods: {
+        addTodo () {
+            var value = this.newTodo && this.newTodo.trim()
+            if (!value) {
+                return
+            }
+            this.$emit('add:todo', value);
+            this.clearInput();
+        },
+        clearInput () {
+            this.newTodo = '';
+        }
+    },
+
+}
+</script>
+
+<style scoped>
+h1 {
+	position: absolute;
+	top: -155px;
+	width: 100%;
+	font-size: 100px;
+	font-weight: 100;
+	text-align: center;
+	color: rgba(175, 47, 47, 0.15);
+	-webkit-text-rendering: optimizeLegibility;
+	-moz-text-rendering: optimizeLegibility;
+	text-rendering: optimizeLegibility;
+}
+
+.new-todo{
+	position: relative;
+	margin: 0;
+	width: 100%;
+	font-size: 24px;
+	font-family: inherit;
+	font-weight: inherit;
+	line-height: 1.4em;
+	border: 0;
+	color: inherit;
+	padding: 16px 16px 16px 60px;
+	border: 0;
+	background: rgba(0, 0, 0, 0.003);
+	box-shadow: inset 0 -2px 1px rgba(0,0,0,0.03);
+	box-sizing: border-box;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+}
+</style>
